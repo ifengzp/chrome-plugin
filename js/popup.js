1c42f742ffebc7d3cea48728991fe67269e1dd4f
Aaron <ifengzp@163.com>
2019-09-07 18:35:01 +0800

    temp

---
+++ js/popup.js
@@ -0,0 +1,29 @@
+console.log(2222)
+
+window.onload = function() {
+    sendMessageToContentScript('你好，我是popup！', (response) => {
+        if(response) alert('收到来自content-script的回复：'+response);
+    });
+    window.close()
+}
+
+// 向content-script主动发送消息
+function sendMessageToContentScript(message, callback)
+{
+	getCurrentTabId((tabId) =>
+	{
+		chrome.tabs.sendMessage(tabId, message, function(response)
+		{
+			if(callback) callback(response);
+		});
+	});
+}
+
+// 获取当前选项卡ID
+function getCurrentTabId(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		if(callback) callback(tabs.length ? tabs[0].id: null);
+	});
+}
